840eef5820cb2ddb01d452defb9132606cfdd5dd
RubenPerezCastrillo <ruben.perez.castrillo@gmail.com>
2024-09-27 13:26:26 +0000

    First commit with app.js done in console

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our", "your", "my", "a"];
+let adj = ["great", "big", "perfect", "correct", "nice"];
+let noun = ["jogger", "racoon", "giant", "beast", "ninja"];
+let ext = [".com", ".es", ".net", ".io", ".org"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generarCombinaciones() {
+  const combinaciones = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  for (const pronombre of pronoun) {
+    for (const adjetivo of adj) {
+      for (const nombre of noun) {
+        for (const extension of ext) {
+          const combinacion = `${pronombre}${adjetivo}${nombre}${extension}`;
+          combinaciones.push(combinacion);
+        }
+      }
+    }
+  }
+  console.log (combinaciones);
+}
+
+generarCombinaciones();
